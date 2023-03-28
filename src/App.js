941b7b823d003f9eb61437798bf1f42359a55838
Caio Ferraz <caioferrazalmeida.27@gmail.com>
2023-03-28 15:09:08 -0300

    Configurando e fazendo deploy

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <div className="App">
 
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <Routes>
 
           <Route path="/" element={<InicialPage />} />
